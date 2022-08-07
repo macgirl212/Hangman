468471b8d9a19779c28aae49385d3e366750e12a
Melody Davis <piano.fan122@gmail.com>
2022-08-06 21:46:49 -0400

    added some basic ui

--- Challenge3/ViewController.swift
+++ Challenge3/ViewController.swift
@@ -8,6 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var remainingTriesLabel: UILabel!
+    var word: UITextField!
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .lightGray
+        
+        remainingTriesLabel = UILabel()
+        remainingTriesLabel.translatesAutoresizingMaskIntoConstraints = false
+        remainingTriesLabel.textAlignment = .right
+        remainingTriesLabel.text = "Remaining Tries: 7"
+        view.addSubview(remainingTriesLabel)
+        
+        word = UITextField()
+        word.translatesAutoresizingMaskIntoConstraints = false
+        word.textAlignment = .center
+        word.placeholder = "Word appears here"
+        word.font = UIFont.systemFont(ofSize: 30)
+        word.isUserInteractionEnabled = false
+        view.addSubview(word)
+        
+        NSLayoutConstraint.activate([
+            remainingTriesLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            remainingTriesLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            
+            word.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            word.topAnchor.constraint(equalTo: remainingTriesLabel.bottomAnchor, constant: 10)
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
