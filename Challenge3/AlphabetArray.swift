e567e950d6c2ffbeba79173e26de5989e915eed7
Melody Davis <piano.fan122@gmail.com>
2022-08-07 22:12:33 -0400

    finished basic ui

---
+++ Challenge3/AlphabetArray.swift
@@ -0,0 +1,10 @@
+//
+//  AlphabetArray.swift
+//  Challenge3
+//
+//  Created by Melody Davis on 8/7/22.
+//
+
+import Foundation
+
+let alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
--- Challenge3/ViewController.swift
+++ Challenge3/ViewController.swift
@@ -31,13 +31,45 @@ class ViewController: UIViewController {
         word.isUserInteractionEnabled = false
         view.addSubview(word)
         
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
+        
         NSLayoutConstraint.activate([
             remainingTriesLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             remainingTriesLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            remainingTriesLabel.bottomAnchor.constraint(equalTo: remainingTriesLabel.topAnchor, constant: 30),
             
             word.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            word.topAnchor.constraint(equalTo: remainingTriesLabel.bottomAnchor, constant: 10)
+            word.topAnchor.constraint(equalTo: remainingTriesLabel.bottomAnchor),
+            
+            buttonsView.widthAnchor.constraint(equalToConstant: 750),
+            buttonsView.heightAnchor.constraint(equalToConstant: 128),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: word.bottomAnchor, constant: 10),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -100)
         ])
+        
+        let width = 750/13
+        let height = 80
+        
+        for row in 0..<2 {
+            for column in 0..<13 {
+                let index = row * 13 + column
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+                letterButton.setTitle(alphabetArray[index], for: .normal)
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor.blue.cgColor
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+                
+                let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView.addSubview(letterButton)
+                letterButtons.append(letterButton)
+            }
+        }
     }
 
     override func viewDidLoad() {
@@ -45,6 +77,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        
+        print(buttonTitle)
+        // search the chosen word for a match with the button title
+        
+        sender.isHidden = true
+    }
 
 }
 
